18fe6431923a969ae5f0b78cf3abb08dc6269c0a
LuizHenriqueMarinho <luizhenriquemarinhofga@gmail.com>
2022-08-30 15:14:33 -0300

    coloquei o Number() nos exercicios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -15,10 +15,10 @@ Obs: O true ou false vai depender dos números inseridos e do resultado das oper
 
 let numero1
 let numero2
-console.log(numero1= prompt("digite o primeiro número"))
-console.log(numero2= prompt("digite o segundo número"))
+console.log(numero1 = Number(prompt("digite o primeiro número")))
+console.log(numero2 = Number(prompt("digite o segundo número")))
 
-console.log("O primeiro numero é maior que segundo?", numero1>numero2)
-console.log("O primeiro numero é igual ao segundo?", numero1===numero2)
-console.log("O primeiro numero é divisível pelo segundo?", (numero1%numero2) === 0)
-console.log("O primeiro segundo é divisível pelo primeiro?", (numero2%numero1) === 0)
+console.log("O primeiro numero é maior que segundo?", numero1 > numero2)
+console.log("O primeiro numero é igual ao segundo?", numero1 === numero2)
+console.log("O primeiro numero é divisível pelo segundo?", (numero1 % numero2) === 0)
+console.log("O primeiro segundo é divisível pelo primeiro?", (numero2 % numero1) === 0)
